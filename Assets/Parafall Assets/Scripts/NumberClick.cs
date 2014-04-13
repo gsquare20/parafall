3cef99ec19e632770c16a9a20bf8186b5b9e00db
Gaurav <grvsachdeva@gmail.com>
2014-04-13 23:46:44 +0530

    Parafall First Commit

---
+++ Assets/Parafall Assets/Scripts/NumberClick.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NumberClick : MonoBehaviour {
+
+	public GUIText testGuiText = null;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown() {
+		Debug.Log ("Mouse down on " + this.name);
+		testGuiText.text = "Mouse down on " + this.name;
+		this.guiText.color = Color.red;
+	}
+
+	void OnMouseUp() {
+		Debug.Log ("Mouse up on " + this.name);
+		testGuiText.text = "Mouse up on " + this.name;
+		this.guiText.color = Color.white;
+	}
+}
