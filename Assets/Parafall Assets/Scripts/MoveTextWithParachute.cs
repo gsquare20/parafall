8a26afd21a4f109a2fc1c82e9cc5565da93a0c9e
Gaurav <grvsachdeva@gmail.com>
2014-04-30 01:45:33 +0530

    Adding additional scripts and a prefab

---
+++ Assets/Parafall Assets/Scripts/MoveTextWithParachute.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTextWithParachute : MonoBehaviour {
+
+	public Transform target;
+	Camera cam;
+	public Vector3 offset = Vector3.up;
+
+	// Use this for initialization
+	void Start () {
+		cam = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = cam.WorldToViewportPoint(target.position + offset);
+	}
+}
+		                                                
---
+++ Assets/Parafall Assets/Scripts/ParachuteSpawner.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParachuteSpawner : MonoBehaviour {
+
+	public float spawnRate = .5f;
+
+	public float firstInvokeTime = .5f;
+
+	public GameObject parachuteGO;
+
+	private Transform myTransform;
+
+	// Use this for initialization
+	void Start () {
+		myTransform = this.transform;
+		InvokeRepeating("spawnParachute", firstInvokeTime, spawnRate);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void spawnParachute() {
+		GameObject tempGO = (GameObject)Instantiate (parachuteGO, myTransform.position, myTransform.rotation);
+
+		GUIText childGuiTextObj = tempGO.transform.GetChild (0).guiText;
+		float randomNum = Random.Range (0F, 1000F);
+		int randomInt = (int)randomNum;
+		//Debug.Log ("Generated random number : " + randomInt.ToString());
+		childGuiTextObj.text = randomInt.ToString ();
+		/*Vector3 tempGONormalizedPosition = Camera.mainCamera.WorldToViewportPoint(tempGO.transform.position);
+		Debug.Log("tempGO nomalized position y : "  + tempGONormalizedPosition.y);
+		tempGONormalizedPosition.y = tempGONormalizedPosition.y + 10F;
+		childGuiTextObj.transform.position =  tempGONormalizedPosition;*/
+	}
+
+}
